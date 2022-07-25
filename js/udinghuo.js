8d1d623418c68c60c1f43d0b51a5dba701ef04bf
Ywr470832459 <48242409+Ywr470832459@users.noreply.github.com>
2022-07-25 14:01:03 +0800

    Create udinghuo.js

---
+++ js/udinghuo.js
@@ -0,0 +1,10 @@
+__app.define('home_VM_Extend.js', function () { 
+    var extend = { 
+        //页面挂载后事件，能获取到dom元素 
+        mounted: function (data) { 
+            console.log(data)
+            debugger 
+        }, 
+    }; 
+    return extend; 
+});
